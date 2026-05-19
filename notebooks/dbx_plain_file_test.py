b5c0cac739b3e797d295d70dc5685b52af1f2402
lainamohemedtaha@gmail.com <lainamohemedtaha@gmail.com>
2026-05-19 13:08:44 +0000

    hhhhhhh testing oku

---
+++ notebooks/dbx_plain_file_test.py
@@ -0,0 +1,5 @@
+  def greet(name):
+          return f"hello {name}, from Databricks"
+
+     print(greet("dbx"))
+     
---
+++ notebooks/dbx_plain_file_test1.py
@@ -0,0 +1,4 @@
+ def greet(name):
+         return f"hello {name}, from Databricks"
+
+     print(greet("dbx"))
